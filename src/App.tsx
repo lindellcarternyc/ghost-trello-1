17c50c20f6d5eb661a56258c988502b1dfaebd0e
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-25 23:02:03 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        ghost-trello
+      </div>
+    )
+  }
+}
+
+export default App
