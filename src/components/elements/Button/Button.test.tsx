2a71a4be2f3631831cec5b57d11cbd1eb5ef7699
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-26 15:01:38 -0500

    Updated tests to use test renderer

--- src/components/elements/Button/Button.test.tsx
+++ src/components/elements/Button/Button.test.tsx
@@ -2,16 +2,20 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Button } from './button'
 
+import * as renderer from 'react-test-renderer'
+
 it('renders a button without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<Button />, div)
 })
 
 it('accepts a content prop (string) and renders it', () => {
-  const div = document.createElement('div')
-  const button = <Button content='Button' />
-  expect(button.props.content).toBe('Button')
-  
+  const button = renderer.create(
+    <Button content='Button' />
+  )
+
+  const tree = button.toJSON()
+  expect(tree).toMatchSnapshot()
 })
 
 // it('always renders')
