532b3dbdefcd56b658987f8ea88d104bdec527cb
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-26 15:29:05 -0500

    Reformatted button test

--- src/components/elements/Button/Button.test.tsx
+++ src/components/elements/Button/Button.test.tsx
@@ -3,23 +3,17 @@ import * as ReactDOM from 'react-dom'
 import { Button } from './button'
 
 import * as renderer from 'react-test-renderer'
-import { shallow } from 'enzyme'
 
 it('renders a button without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<Button />, div)
 })
 
-it('accepts a content prop (string) and renders it', () => {
+it('accepts a content prop (string) and renders it (capitalized by default)', () => {
   const button = renderer.create(
     <Button content='Button' />
   )
 
   const tree = button.toJSON()
   expect(tree).toMatchSnapshot()
-})
-
-it('should render text capitalized by default', () => {
-  const button = shallow(<Button content='button' />)
-  expect(button.text()).toEqual('BUTTON')
 })
