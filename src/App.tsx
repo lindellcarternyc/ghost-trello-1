373598d8b1dd74b8d07515b46a84df48dcd932c9
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-26 19:09:52 -0500

    Added Story component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import { Button } from './components/elements/Button/Button'
+import { Story } from './components/views/Story/Story'
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,9 @@ class App extends React.Component {
       <div>
         <h2>ghost-trello</h2>
         <Button content='Hello World'/>
+        <br />
+        <br />
+        <Story />
       </div>
     )
   }
