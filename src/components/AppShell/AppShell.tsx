0ee7f8f0d05d01973f021c06f0b9e837c71a1ceb
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-28 15:10:44 -0500

    Added state for handling menu and search open and click handlers

--- src/components/AppShell/AppShell.tsx
+++ src/components/AppShell/AppShell.tsx
@@ -3,23 +3,37 @@ import * as React from 'react'
 interface AppShellProps {
   children?: JSX.Element[]
 }
+interface AppShellState {
+  menuOpen: boolean
+  searchOpen: boolean
+}
 
 import './AppShell.styles.css'
 
-export const AppShell = (props: AppShellProps) => {
-  const { children } = props
-  return (
-    <div className='appShell'>
-      <div className='appShell--menuButtons'>
-        <button className='appShell--menuButton'>
-          <i className='fas fa-plus' />
-        </button>
-        <br />
-        <button className='appShell--menuButton'>
-          <i className='fas fa-search' />
-        </button>
+export class AppShell extends React.Component<AppShellProps, AppShellState> {
+  state = {
+    menuOpen: false,
+    searchOpen: false
+  }
+
+  openMenu = () => this.setState({menuOpen: true})
+  openSearch = () => this.setState({searchOpen: true})
+
+  render() {
+    const { children } = this.props
+    return (
+      <div className='appShell'>
+        <div className='appShell--menuButtons'>
+          <button className='appShell--menuButton' onClick={this.openMenu}>
+            <i className='fas fa-plus' />
+          </button>
+          <br />
+          <button className='appShell--menuButton' onClick={this.openSearch}>
+            <i className='fas fa-search' />
+          </button>
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
-  )
+    )
+  }
 }
