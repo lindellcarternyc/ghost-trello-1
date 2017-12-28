8e739455dc8678d4b7b18822a2fe318c8fed054c
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-27 21:55:31 -0500

    Updated story test to use mock story

--- src/components/views/Story/Story.test.tsx
+++ src/components/views/Story/Story.test.tsx
@@ -4,18 +4,10 @@ import * as renderer from 'react-test-renderer'
 
 import { Story } from './Story'
 
+import { generateDescription } from '../../../mocks/mock-story'
+
 const MOCK_STORY = {
-  description: 
-    // tslint:disable-next-line:quotemark
-    "Mount Everest, also known in Nepal as Sagarmāthā and in Tibet as Chomolungma, is Earth's highest mountain. " +
-    // tslint:disable-next-line:quotemark
-    "It is located in the Mahalangur mountain range in Nepal. Its peak is 8,848 metres (29,029 ft) above sea level. " +
-    // tslint:disable-next-line:quotemark
-    "It is not the furthest summit from the centre of the Earth. That honour goes to Mount Chimborazo," +
-    // tslint:disable-next-line:quotemark
-    " in the Andes.[10] The international border between China and Nepal runs across Everest's precise summit point. "
-    // tslint:disable-next-line:quotemark
-    + "Its massif includes neighbouring peaks Lhotse, 8,516 m (27,940 ft);"
+  description: generateDescription()
 }
 
 // describe('Story', () => {
