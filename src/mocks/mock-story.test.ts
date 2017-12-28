f9454852debe07c4a089115dd72102dbfc385255
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-28 11:47:59 -0500

    Added test for generating mock story with image and text

--- src/mocks/mock-story.test.ts
+++ src/mocks/mock-story.test.ts
@@ -1,5 +1,5 @@
 import { 
-  generateTextSection, generateHeaderImage
+  generateTextSection, generateHeaderImage, generateStory
 } from './mock-story'
 
 it('should create a mock story text section', () => {
@@ -21,4 +21,23 @@ it('should create a mock story text section', () => {
 it('should create a mock story header image url', () => {
   const image = generateHeaderImage()
   expect(image).toMatch('http://lorempixel.com/640/480/abstract')
-}
+})
+
+it('should create a mock story', () => {
+  const story = generateStory()
+
+  const { image, text } = story
+  expect(image).toMatch('http://lorempixel.com/640/480/abstract')
+
+  const paragraphGroups = text.split('\n\n\n')
+  const numParagraphGroups = paragraphGroups.length
+  expect(numParagraphGroups).toBeGreaterThan(2)
+  expect(numParagraphGroups).toBeLessThan(6)
+
+  paragraphGroups.forEach(group => {
+    const paragraphs = group.split('\n \r')
+    const numParagraphsInGroup = paragraphs.length
+    expect(numParagraphsInGroup).toBeGreaterThan(3)
+    expect(numParagraphsInGroup).toBeLessThan(11)
+  })
+})
