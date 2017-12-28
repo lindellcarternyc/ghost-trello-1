a11da43ed93d2361b86a2dfad72364036b76d324
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-27 22:13:12 -0500

    Updated story tests and snapshots to use mock story text

--- src/components/views/Story/Story.test.tsx
+++ src/components/views/Story/Story.test.tsx
@@ -4,23 +4,23 @@ import * as renderer from 'react-test-renderer'
 
 import { Story } from './Story'
 
-import { generateDescription } from '../../../mocks/mock-story'
+import { generateText } from '../../../mocks/mock-story'
 
 const MOCK_STORY = {
-  description: generateDescription()
+  text: generateText()
 }
 
 // describe('Story', () => {
 it('renders a story without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Story description={MOCK_STORY.description}/>, div)
+  ReactDOM.render(<Story text={MOCK_STORY.text}/>, div)
 })
 
-it('accepts a description prop that\'s a string', () => {
+it('accepts a text prop that is a string and renders it', () => {
   const story = renderer.create(
-    <Story description={MOCK_STORY.description} />
+    <Story text={MOCK_STORY.text}/>
   )
+
   const tree = story.toJSON()
   expect(tree).toMatchSnapshot()
 })
-// })
