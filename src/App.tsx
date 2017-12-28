40e3641071a6c0beec05e22acfa9ef24c59337cb
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-28 14:46:31 -0500

    Wrapped contents in app shell

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import * as React from 'react'
 import { Button } from './components/elements/Button/Button'
 import { Story } from './components/views/Story/Story'
 
+import { AppShell } from './components/AppShell/AppShell'
+
 import { generateStory } from './mocks/mock-story'
 
 const MOCK_STORY = generateStory()
@@ -10,13 +12,13 @@ const MOCK_STORY = generateStory()
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <AppShell>
         <h2>ghost-trello</h2>
         <Button content='Hello World'/>
         <br />
         <br />
         <Story story={MOCK_STORY}/>
-      </div>
+      </AppShell>
     )
   }
 }
