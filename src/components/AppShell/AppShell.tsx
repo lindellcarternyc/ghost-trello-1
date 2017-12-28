8ec7e5891c6b0d924a523bbb1c11ba80958b8aef
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-28 15:00:55 -0500

    Added buttons and styles to App shell

--- src/components/AppShell/AppShell.tsx
+++ src/components/AppShell/AppShell.tsx
@@ -4,10 +4,21 @@ interface AppShellProps {
   children?: JSX.Element[]
 }
 
+import './AppShell.styles.css'
+
 export const AppShell = (props: AppShellProps) => {
   const { children } = props
   return (
-    <div>
+    <div className='appShell'>
+      <div className='appShell--menuButtons'>
+        <button className='appShell--menuButton'>
+          <i className='fas fa-plus' />
+        </button>
+        <br />
+        <button className='appShell--menuButton'>
+          <i className='fas fa-search' />
+        </button>
+      </div>
       {children}
     </div>
   )
